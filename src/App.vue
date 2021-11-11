a7c7f5bc6e5e091075388756109c64a87b0724b1
Evgeniy Kirianov <85870790+EvgeniyKiryanow@users.noreply.github.com>
2021-11-11 22:59:04 +0200

    create header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
-   app
+   <Header />
+   <div class="body"></div>
+   <Footer />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+export default {
+  name: 'TitlePage',
+  components: {Header,Footer}
+}
 
 </script>
 
 <style lang="scss">
-@import "styles/media.scss"
+@import "styles/media.scss";
+@import "styles/mixins.scss";
+@import "styles/reset.scss";
+@import "styles/variables.scss";
+html {
+  font-size: 8px;
+}
+body {
+  background-color: $background;
+}
+.body {
+height: 500px;
+}
 </style>
