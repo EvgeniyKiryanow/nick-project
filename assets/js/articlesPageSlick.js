e85ea225fd2bf5aff65c040cd5d1cb90c36d580a
Misha Rozdorozhniuk <applemisharoz2003gmail.com@MacBook-Pro-Misha.local>
2021-11-18 23:08:58 +0200

    add first part of modal

--- assets/js/articlesPageSlick.js
+++ assets/js/articlesPageSlick.js
@@ -1,29 +1,39 @@
-  $(document).ready(function () {
-    if ($(window).width() < 376 ) {
-        $('.articles__body').addClass("slick");
-        // Boxes
-        $('.slick').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-            mobileFirst:true,
-        });
-    } else {
-        $('.slick').slick('unslick')
-        $('.articles__body').removeClass("slick");
-    }
-    $(window).resize(function () {
-        if ($(window).width() < 376 ) {
-            $('.articles__body').addClass("slick");
-            $('.slick').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                mobileFirst:true,
-            });
-        } else {
-            $('.slick').slick('unslick')
-            $('.articles__body').removeClass("slick");
-        }
-    });
-});
+  $(document).ready(function() {
+      if ($(window).width() < 376) {
+          $('.articles__body').addClass("slick");
+          // Boxes
+          $('.slick').slick({
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: true,
+              mobileFirst: true,
+          });
+      } else {
+          $('.slick').slick('unslick')
+          $('.articles__body').removeClass("slick");
+      }
+      $(window).resize(function() {
+          if ($(window).width() < 376) {
+              $('.articles__body').addClass("slick");
+              $('.slick').slick({
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  dots: true,
+                  mobileFirst: true,
+              });
+          } else {
+              $('.slick').slick('unslick')
+              $('.articles__body').removeClass("slick");
+          }
+      });
+  });
+
+  (function($) {
+      $(function() {
+          $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+              $(this).addClass('active').siblings().removeClass('active')
+                  .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+          })
+
+      })
+  })(jQuery)
--- assets/js/headerMenuToggle.js
+++ assets/js/headerMenuToggle.js
@@ -1,9 +1,17 @@
-$('.header__menu').click(function () {
+$('.header__menu').click(function() {
     $('.lines').toggleClass("active")
 })
-$('.modal-backdrop').click(function(){
+$('.modal-backdrop').click(function() {
     $('.lines').removeClass('active')
 })
-$('#exampleModal').click(function(){
-    $('.lines').removeClass('active')
-})
+$('#exampleModal').click(function() {
+        $('.lines').removeClass('active')
+    })
+    (function($) {
+        $(function() {
+            $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+                $(this).addClass('active').siblings().removeClass('active')
+                    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            })
+        })
+    })(jQuery)
