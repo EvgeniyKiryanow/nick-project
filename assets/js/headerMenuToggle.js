8b6796f0f27d9550a83f47544a104fa3fe68224c
Misha Rozdorozhniuk <applemisharoz2003gmail.com@MacBook-Pro-Misha.local>
2021-11-22 15:15:13 +0200

    add second part of modal for fast ordering with adaptive extention

--- assets/js/headerMenuToggle.js
+++ assets/js/headerMenuToggle.js
@@ -7,16 +7,39 @@ $('.modal-backdrop').click(function() {
 $('#exampleModal').click(function() {
     $('.lines').removeClass('active')
 })
-$('.modal-first-step').click(function(){
-    $('.modal-first-step').addClass('active')
+$('.modal-first-step').click(function() {
+    $('.modal-first-step').addClass('active-tab')
     $('.test1').addClass('active')
-    $('.modal-second-step').removeClass('active')
+    $('.modal-second-step').removeClass('active-tab')
     $('.test2').removeClass('active')
 })
-$('.modal-second-step').click(function(){
-    $('.modal-second-step').addClass('active')
+$('.modal-second-step').click(function() {
+    $('.modal-second-step').addClass('active-tab')
     $('.test2').addClass('active')
-    $('.modal-first-step').removeClass('active')
+    $('.modal-first-step').removeClass('active-tab')
     $('.test1').removeClass('active')
 })
+$('.modal-second-step').click(function() {
+    $('.modal-body').removeClass('top')
+})
+$('.modal-first-step').click(function() {
+    $('.modal-body').addClass('top')
+})
 
+
+$('.learning-order-select').click(function() {
+    $('.learning-order-select').addClass('active')
+    $('.learning-order-select').addClass('active-btn')
+    $('.first-part-of-modal').addClass('active')
+    $('.teachers-order-select').removeClass('active')
+    $('.teachers-order-select').removeClass('active-btn')
+    $('.second-part-of-modal').removeClass('active')
+})
+$('.teachers-order-select').click(function() {
+    $('.teachers-order-select').addClass('active active-btn')
+    $('.teachers-order-select').addClass('active-btn')
+    $('.second-part-of-modal').addClass('active')
+    $('.learning-order-select').removeClass('active')
+    $('.learning-order-select').removeClass('active-btn')
+    $('.first-part-of-modal').removeClass('active')
+})
