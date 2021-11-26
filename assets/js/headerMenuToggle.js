fae1e507be9f5cadd69f8cb53ec1b1ac66317496
Misha Rozdorozhniuk <applemisharoz2003gmail.com@MacBook-Pro-Misha.local>
2021-11-26 16:27:09 +0200

    last changes

--- assets/js/headerMenuToggle.js
+++ assets/js/headerMenuToggle.js
@@ -5,24 +5,24 @@ $('.modal-backdrop').click(function() {
     $('.lines').removeClass('active')
 })
 $('#exampleModal').click(function() {
-    $('.lines').removeClass('active')
-})
-$('.modal-first-step').click(function() {
-    $('.modal-first-step').addClass('active-tab')
-    $('.test1').addClass('active')
-    $('.modal-second-step').removeClass('active-tab')
-    $('.test2').removeClass('active')
-})
+        $('.lines').removeClass('active')
+    })
+    // $('.modal-first-step-btn').click(function() {
+    //     $('.modal-first-step-btn').addClass('active-tab')
+    //     $('.test1').addClass('active')
+    //     $('.modal-second-step').removeClass('active-tab')
+    //     $('.test2').removeClass('active')
+    // })
 $('.modal-second-step').click(function() {
+    // $('.modal-first-step-btn').removeClass('active-tab')
+    $('.test1').removeClass('active')
     $('.modal-second-step').addClass('active-tab')
     $('.test2').addClass('active')
-    $('.modal-first-step').removeClass('active-tab')
-    $('.test1').removeClass('active')
 })
 $('.modal-second-step').click(function() {
     $('.modal-body').removeClass('top')
 })
-$('.modal-first-step').click(function() {
+$('.modal-first-step-btn').click(function() {
     $('.modal-body').addClass('top')
 })
 
@@ -43,3 +43,62 @@ $('.teachers-order-select').click(function() {
     $('.learning-order-select').removeClass('active-btn')
     $('.first-part-of-modal').removeClass('active')
 })
+
+function limitChars(myObject, max, leftChars) {
+    $(myObject).keydown(function() {
+        var count = $(this).val().length;
+        var num = max - count;
+
+        if (num > 0) {
+            $(leftChars).text('Можно ввести символов: ' + num);
+            $(this).removeClass('type');
+        } else {
+            $(leftChars).text('Достигнут лимит символов');
+            $(this).addClass('type');
+        }
+    });
+}
+
+$(document).ready(function() {
+    var myObject = 'textarea#topic';
+    var mySecObject = 'textarea#description'
+    var myThirdObject = 'textarea#description.description.teacher'
+
+    var max = 250;
+    var descMax = 1000;
+    var descTeacherMax = 1000;
+
+    var leftChars = '#leftChars';
+    var leftCharsDesc = '#leftCharsDesc';
+    var leftCharsDescTeachet = '#leftCharsTeach';
+
+    limitChars(myObject, max, leftChars);
+    limitChars(mySecObject, descMax, leftCharsDesc);
+    limitChars(myThirdObject, descTeacherMax, leftCharsDescTeachet);
+});
+$('#staticBackdropBtn').attr('disabled', 'disabled');
+
+
+$(document).ready(function() {
+    $('.field input').keyup(function() {
+        var empty = false;
+
+        $('.field input').each(function() {
+            if ($(this).val().length == 0) {
+                $("#staticBackdropBtn").attr("disabled", false);
+                empty = true;
+            }
+            if ($(".label-check-inp").click(function() {
+                    $("#staticBackdropBtn").attr("disabled", false);
+                })) {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#staticBackdropBtn').attr('disabled', 'disabled');
+        } else {
+            $('#staticBackdropBtn').attr('disabled', empty);
+        }
+    });
+});
