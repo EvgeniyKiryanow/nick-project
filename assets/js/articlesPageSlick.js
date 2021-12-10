f63f5ff5525c02057fa00df9338a16758d5345b3
Misha Rozdorozhniuk <applemisharoz2003gmail.com@MacBook-Pro-Misha.local>
2021-12-10 23:28:35 +0200

    add test version of home page desctop

--- assets/js/articlesPageSlick.js
+++ assets/js/articlesPageSlick.js
@@ -4,15 +4,14 @@ function supportsWebp() {
     if (!self.createImageBitmap) return false;
 
     const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
-    const blob = fetch(webpData).then(function (r) { return r.blob() });
-    return createImageBitmap(blob).then(function () { return true }, function () { return false });
+    const blob = fetch(webpData).then(function(r) { return r.blob() });
+    return createImageBitmap(blob).then(function() { return true }, function() { return false });
 }
 
-(function () {
+(function() {
     if (supportsWebp()) {
         return
-    }
-    else {
+    } else {
         alert('Этот браузер не поддерживает фотографии с разширением .webp,можете скопировать ссылку,и скачать браузер который поддерживает: https://www.google.com.ua; https://www.mozilla.org; https://www.opera.com');
     }
 })();
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,26 +1,92 @@
-$(document).ready(function() {
-    if ($(window).width() > 376) {
-        $('.home__body-slider').addClass("slick");
-        $('.slick').slick({
-            slidesToShow: 4,
-            slidesToScroll: 1,
-        });
-    } else {
-        $('.slick').slick('unslick')
-        $('.home__body-slider').removeClass("slick");
-    }
-    $(window).resize(function() {
-        if ($(window).width() < 376) {
-            $('.home__body-slider').addClass("slick");
-            $('.slick').slick({
+$('.home__body-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                mobileFirst: true,
-            });
-        } else {
-            $('.slick').slick('unslick')
-            $('.home__body-slider').removeClass("slick");
+                slidesToScroll: 1
+            }
         }
-    });
+    ]
 });
+
+$('#question1').click(function() {
+    $('#answer1').toggleClass("show-answer")
+    $('#answer1').removeClass("hidden-answer")
+})
+
+$('#question2').click(function() {
+    $('#answer2').toggleClass("show-answer")
+    $('#answer2').removeClass("hidden-answer")
+})
+
+$('#question3').click(function() {
+    $('#answer3').toggleClass("show-answer")
+    $('#answer3').removeClass("hidden-answer")
+})
+
+$('#question4').click(function() {
+    $('#answer4').toggleClass("show-answer")
+    $('#answer4').removeClass("hidden-answer")
+})
+
+$('#question5').click(function() {
+    $('#answer5').toggleClass("show-answer")
+    $('#answer5').removeClass("hidden-answer")
+})
+
+$('#question6').click(function() {
+    $('#answer6').toggleClass("show-answer")
+    $('#answer6').removeClass("hidden-answer")
+})
+
+
+
+$('#cross1').click(function() {
+    $('#answer1').addClass("hidden-answer")
+    $('#answer1').removeClass("show-answer")
+})
+
+$('#cross2').click(function() {
+    $('#answer2').addClass("hidden-answer")
+    $('#answer2').removeClass("show-answer")
+})
+
+$('#cross3').click(function() {
+    $('#answer3').addClass("hidden-answer")
+    $('#answer3').removeClass("show-answer")
+})
+
+$('#cross4').click(function() {
+    $('#answer4').addClass("hidden-answer")
+    $('#answer4').removeClass("show-answer")
+})
+
+$('#cross5').click(function() {
+    $('#answer5').addClass("hidden-answer")
+    $('#answer5').removeClass("show-answer")
+})
+
+$('#cross6').click(function() {
+    $('#answer6').addClass("hidden-answer")
+    $('#answer6').removeClass("show-answer")
+})
