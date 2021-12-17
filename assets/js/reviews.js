d0b74f5c481691e7367e74b79570965387b9b8ae
Misha Rozdorozhniuk <applemisharoz2003gmail.com@MacBook-Pro-Misha.local>
2021-12-17 18:25:07 +0200

    add branch for reviews page

---
+++ assets/js/reviews.js
@@ -0,0 +1,44 @@
+// $(function (){
+// 	var reviewLength = $('.reviews-page-wr .reviews-item-wr .reviews-item').length;
+// 	if(reviewLength <= 12){
+// 		$('.reviews-page-wr .reviews-more').hide();
+// 	}
+
+// 	$('.reviews-page-wr .reviews-more').on('click', function () {
+// 		$('.reviews-page-wr .reviews-item-wr .reviews-item:nth-child(n+13)').slideToggle();
+// 		$(this).hide();
+// 	});
+
+// 	resizeController(600, function() {
+// 		$('.reviews-page-wr .reviews-item-wr').slick({
+// 			arrows: true,
+// 			dots: true,
+// 			infinite: true,
+// 			speed: 800,
+// 			rows: 2,
+// 			autoplay: false,
+// 			draggable: true,
+// 			prevArrow: $(".slider-prev"),
+// 	        nextArrow: $(".slider-next"),
+// 	        appendDots: $(".reviews-slider__nav-wrap"),
+// 			slidesToShow: 1
+// 		});
+// 		$('.navs-wr')
+// 	}, function(){
+// 		$('.reviews-page-wr .reviews-item-wr.slick-initialized').slick('unslick');
+// 	});
+// });
+
+$(document).ready(function() {
+    $('.reviews-item-wr').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
