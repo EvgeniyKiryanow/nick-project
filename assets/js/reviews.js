b6080d20c97b037c28af51367a3f0dffb7f0a507
Misha Rozdorozhniuk <applemisharoz2003gmail.com@MacBook-Pro-Misha.local>
2021-12-20 13:25:30 +0200

    add mobile and tablet version on reviews page

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -1,42 +1,11 @@
-// $(function (){
-// 	var reviewLength = $('.reviews-page-wr .reviews-item-wr .reviews-item').length;
-// 	if(reviewLength <= 12){
-// 		$('.reviews-page-wr .reviews-more').hide();
-// 	}
-
-// 	$('.reviews-page-wr .reviews-more').on('click', function () {
-// 		$('.reviews-page-wr .reviews-item-wr .reviews-item:nth-child(n+13)').slideToggle();
-// 		$(this).hide();
-// 	});
-
-// 	resizeController(600, function() {
-// 		$('.reviews-page-wr .reviews-item-wr').slick({
-// 			arrows: true,
-// 			dots: true,
-// 			infinite: true,
-// 			speed: 800,
-// 			rows: 2,
-// 			autoplay: false,
-// 			draggable: true,
-// 			prevArrow: $(".slider-prev"),
-// 	        nextArrow: $(".slider-next"),
-// 	        appendDots: $(".reviews-slider__nav-wrap"),
-// 			slidesToShow: 1
-// 		});
-// 		$('.navs-wr')
-// 	}, function(){
-// 		$('.reviews-page-wr .reviews-item-wr.slick-initialized').slick('unslick');
-// 	});
-// });
-
 $(document).ready(function() {
     if ($(window).width() < 600) {
         $('.reviews-item-wr').addClass("slick");
         $('.slick').slick({
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             dots: true,
-            mobileFirst: true,
+            infinite: true,
         });
     } else {
         $('.slick').slick('unslick')
@@ -46,10 +15,10 @@ $(document).ready(function() {
         if ($(window).width() < 600) {
             $('.reviews-item-wr').addClass("slick");
             $('.slick').slick({
-                slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 dots: true,
-                mobileFirst: true,
+                infinite: true,
             });
         } else {
             $('.slick').slick('unslick')
@@ -57,17 +26,3 @@ $(document).ready(function() {
         }
     });
 });
-
-// $(document).ready(function() {
-//     $('.reviews-item-wr').slick({
-//         slidesToShow: 2,
-//         slidesToScroll: 1,
-//         responsive: [{
-//             breakpoint: 600,
-//             settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 1
-//             }
-//         }]
-//     });
-// });
